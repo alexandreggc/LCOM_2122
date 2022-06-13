f544f7aa8c7548ef6006864d8874cfe9824d4f92
Tiago Branquinho <up202005567@edu.fe.up.pt>
2022-06-13 03:31:31 +0100

    Updated Font

--- proj/src/project/src/entities.c
+++ proj/src/project/src/entities.c
@@ -8,7 +8,7 @@
 #include "wall.xpm"
 #include "bomberman.xpm"
 #include "bomberman2.xpm"
-#include "bot2.xpm"
+#include "bot.xpm"
 #include "bomb.xpm"
 #include "bomb2.xpm"
 #include "stone.xpm"
@@ -173,7 +173,7 @@ struct bot{
 
 bot_t* (bot_constructor)(int x, int y){
   bot_t * ret = malloc(sizeof(bot_t));
-  sprite_t *sp = sprite_constructor((const char *const *)bomberman2_xpm);
+  sprite_t *sp = sprite_constructor((const char *const *)bot_xpm);
   sprite_set_pos(sp, x, y);
   ret->sp = sp;
   ret->x_map = 1;
